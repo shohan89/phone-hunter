797845e0ae0458709aa45ab7cd7de38d691f3105
shoh4n <maffin.killer007@gmail.com>
2025-06-17 19:36:46 +0600

    modal added

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-const loadPhones = async (phoneName) => {
+const loadPhones = async (phoneName, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${phoneName}`
   );
   const data = await res.json();
   const phones = data.data;
-  displayPhones(phones);
+  displayPhones(phones, isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isShowAll) => {
   const phoneContainer = document.getElementById("card-container");
   phoneContainer.innerHTML = ""; // clear the container before adding new phones
   //   console.log(phones);
@@ -18,8 +18,12 @@ const displayPhones = (phones) => {
   } else {
     showAllButton.classList.add("hidden");
   }
+  console.log('is show all', isShowAll);
   // showing 0 - 12 search result using slice
-  phones.slice(0, 12).forEach((phone) => {
+  if(!isShowAll){
+    phones = phones.slice(0, 12);
+  }
+  phones.forEach((phone) => {
     const phoneDiv = document.createElement("div");
     phoneDiv.classList = "card bg-gray-100 shadow-sm";
     phoneDiv.innerHTML = `
@@ -29,23 +33,32 @@ const displayPhones = (phones) => {
             alt=${phone.phone_name} />
         </figure>
         <div class="card-body">
-            <h2 class="card-title">${phone.phone_name}</h2>
+            <h2 class="card-title text-center">${phone.phone_name}</h2>
             <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div class="card-actions justify-start">
+                <button onclick="handleShowDetails('${phone.slug}');showDetailsModal.showModal()" class="btn btn-primary">Show Details</button>
+            </div>
         </div>
        `;
     phoneContainer.appendChild(phoneDiv);
   });
   loadingSpinner(false);
 };
+// handle show details button
+const handleShowDetails = async (id) =>{
+    console.log('clicked', id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    console.log(phone);
 
-const handleSearch = () => {
+}
+
+const handleSearch = (isShowAll) => {
   const searchField = document.getElementById("search-input");
   const searchText = searchField.value;
   searchField.value = ""; // clear the box after search
-  loadPhones(searchText);
-  if (searchText === "") {
-    alert("Please enter a phone name to search");
-  }
+  loadPhones(searchText, isShowAll);
   loadingSpinner(true);
 };
 
@@ -59,7 +72,10 @@ const loadingSpinner = (isLoading) =>{
         spinner.classList.add('hidden');
     }
 }
-
+// Show all phones when click show all button
+const showAllPhones = () =>{
+    handleSearch(true);
+}
 
 
 // loadPhones();
