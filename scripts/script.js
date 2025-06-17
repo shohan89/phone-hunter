50601f358243f723b940bc6f3f4c737895599313
shoh4n <maffin.killer007@gmail.com>
2025-06-17 11:37:55 +0600

    added

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,31 @@
-const loadPhones = async () =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
-    const data = await res.json();
-    const phones = data.data;
-    console.log(phones);
-}
+const loadPhones = async () => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=iphone`
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhones(phones);
+};
 
-const displayPhones = phones =>{
-    const phoneContainer = document.getElementById('phone-container');
-    phones.forEach(phone => {
-       console.log(phone); 
-    })
-}
-
-
-
-
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("card-container");
+//   console.log(phones);
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList = "card bg-gray-100 shadow-sm";
+    phoneDiv.innerHTML = `
+        <figure>
+            <img
+            src=${phone.image}
+            alt=${phone.phone_name} />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        </div>
+       `;
+       phoneContainer.appendChild(phoneDiv);
+  });
+};
 
 loadPhones();
